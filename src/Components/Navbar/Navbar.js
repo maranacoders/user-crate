a1f15abcbf3cf1e0d562c662848b547d19351344
harish-sethuraman <32865581+harish-sethuraman@users.noreply.github.com>
2020-07-15 22:48:36 +0530

    Update Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import './Navbar.scss'
+import "./Navbar.scss";
 class Navbar extends Component {
   render() {
     return (
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
       </nav>
     );
   }
