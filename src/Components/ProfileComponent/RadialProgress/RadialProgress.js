013ef1a09b46066d03d0b9189f2037451a2e5f08
harish-sethuraman <32865581+harish-sethuraman@users.noreply.github.com>
2020-07-18 17:22:36 +0530

    Update RadialProgress.js

--- src/Components/ProfileComponent/RadialProgress/RadialProgress.js
+++ src/Components/ProfileComponent/RadialProgress/RadialProgress.js
@@ -1,22 +1,30 @@
-import React, { Component } from "react";
-import Styles from "./RadialProgress.module.scss";
+import React, { Component } from 'react';
+import Styles from './RadialProgress.module.scss';
 
 class RadialProgress extends Component {
   componentDidMount() {
-      const {color}=this.props
-    const percent = parseInt(document.querySelector('.' + Styles.ProgressPieChart).getAttribute('data-percent'))
-    console.log(percent)
-    console.log(360*percent/100)
-    if (360*(percent/100) > 50){
-      document.querySelector('.' + Styles.ProgressPieChart).classList.add(Styles.gt50)
-    //   document.querySelector('.'+Styles.gt50).style.backgroundColor=color
+    const { color } = this.props;
+    const percent = parseInt(
+      document
+        .querySelector('.' + Styles.ProgressPieChart)
+        .getAttribute('data-percent'),
+    );
+    console.log(percent);
+    console.log((360 * percent) / 100);
+    if (360 * (percent / 100) > 50) {
+      document
+        .querySelector('.' + Styles.ProgressPieChart)
+        .classList.add(Styles.gt50);
+      //   document.querySelector('.'+Styles.gt50).style.backgroundColor=color
     }
-    document.querySelector('.' + Styles.PpcProgressFill).style.transform = "rotate(" + 360 * percent / 100 + 'deg)';
+    document.querySelector('.' + Styles.PpcProgressFill).style.transform =
+      'rotate(' + (360 * percent) / 100 + 'deg)';
     // document.querySelector('.'+Styles.PpcProgressFill).style.backgroundColor=color
-    document.querySelector('.' + Styles.PpcPercents + ' span').innerHTML = percent + '%';
+    document.querySelector('.' + Styles.PpcPercents + ' span').innerHTML =
+      percent + '%';
   }
   render() {
-    const {text,color,level}=this.props
+    const { text, color, level } = this.props;
 
     return (
       <>
@@ -26,13 +34,13 @@ class RadialProgress extends Component {
           </div>
           <div className={Styles.PpcPercents}>
             <div className={Styles.PpcPercentsWrapper}>
-              <span style={{color : `${color}`}}>%</span>
-    <p className={Styles.Text} style={{color : `${color}`}}>{text}</p>
-
+              <span style={{ color: `${color}` }}>%</span>
+              <p className={Styles.Text} style={{ color: `${color}` }}>
+                {text}
+              </p>
             </div>
           </div>
         </div>
-
       </>
     );
   }
