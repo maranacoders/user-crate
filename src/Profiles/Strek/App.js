09e8cd52226e19b80f2359e5306df4a7b8877baf
amareshsm <amareshsm13@gmail.com>
2020-07-22 18:41:54 +0530

    Github Component UI Completed

--- src/Profiles/Strek/App.js
+++ src/Profiles/Strek/App.js
@@ -6,20 +6,11 @@ import RadialProgress from '../../Components/ProfileComponent/RadialProgress/Rad
 
 export const StrekHome = () => (
   <div>
-    <Github show="true" username="harish-sethuraman" showRepo="true" />
+    <Github show="true" username="harish-sethuraman" showRepo />
     <Stackoverflow show="true" userid="8780399" />
-    <ProgressBar type="aqua" text="HTML" level="90" />
-    <ProgressBar type="copper" text="CSS" level="50" />
-    <ProgressBar type="candy" text="CPP" level="50" />
-    <ProgressBar type="neon" text="C" level="50" />
-    <ProgressBar type="shine" text="React JS" level="50" />
-    <ProgressBar type="zigzag" text="JS" level="50" />
-    <ProgressBar type="diamond" text="English" level="50" />
-    <ProgressBar type="hearts" text="Tamil" level="50" />
     <ProgressBar type="sparkle" text="Hindi" level="50" />
     <RadialProgress text="new" color="red" level="14" />
     <RadialProgress text="new" color="blue" level="14" />
-
   </div>
 );
 export const StrekName = () => 'harish';
